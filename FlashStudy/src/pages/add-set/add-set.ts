a9050ea9c934178b1f9208845fd6caf3efc1ef02
j0371 <43154079+j0371@users.noreply.github.com>
2018-09-28 17:51:34 -0500

    add set and other stuff (aesthetic)

--- FlashStudy/src/pages/add-set/add-set.ts
+++ FlashStudy/src/pages/add-set/add-set.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddSetPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddSetPage');
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Incomplete',
+      subTitle: 'This button will add the set to your',
+      buttons: ['OK']
+    });
+    alert.present();
   }
-
 }
--- FlashStudy/src/pages/home/home.ts
+++ FlashStudy/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListPage } from '../list/list';
+import { MenuController } from 'ionic-angular'
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,8 @@ import { ListPage } from '../list/list';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private menu: MenuController) {
+    this.menu.swipeEnable(false);
   }
 
   goToCards(){
